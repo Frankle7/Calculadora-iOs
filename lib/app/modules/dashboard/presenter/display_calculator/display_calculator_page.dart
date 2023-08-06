9528476ad3b7d0a35a100c9debe66aa99233c203
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-08-05 21:29:32 -0300

    criação de display do app

---
+++ lib/app/modules/dashboard/presenter/display_calculator/display_calculator_page.dart
@@ -0,0 +1,41 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class DisplayCalculatorPage extends StatelessWidget {
+  final String text;
+
+  const DisplayCalculatorPage({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 432,
+      color: Color.fromRGBO(48, 48, 48, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AutoSizeText(
+              text,
+              minFontSize: 20,
+              maxFontSize: 80,
+              maxLines: 1,
+              textAlign: TextAlign.end,
+              style: const TextStyle(
+                fontWeight: FontWeight.w100,
+                decoration: TextDecoration.none,
+                fontSize: 80,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
