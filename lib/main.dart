db51042a300e67da168172db07caf28d80a8dc37
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-08-05 21:27:43 -0300

    criando rotas para dashboard do app

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:calculator/app/modules/dashboard/presenter/calculator_dashboard_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculatorDashboardPage());
